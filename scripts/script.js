c0112dad75bf299e9fcb85e7b2c933118364babc
Zainuwachtig <74155415+Zainuwachtig@users.noreply.github.com>
2021-01-21 17:38:41 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+var hamburger = document.querySelector(".hamburger");
+var kruisje = document.querySelector(".kruisje");
+
+function openMenu() {
+    var deNav = document.querySelector("header nav");
+    deNav.classList.toggle("toonmenu");
+}
+
+hamburger.addEventListener("click", openMenu);
+kruisje.addEventListener("click", openMenu);
